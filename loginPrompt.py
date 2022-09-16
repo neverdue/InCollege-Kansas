058db7ca4b11c0af0bb38001ac6209989d50f73a
Andy Nguyen <nguyena5@usf.edu>
2022-09-15 23:32:46 -0400

    new function for choosing existing user and new user

--- loginPrompt.py
+++ loginPrompt.py
@@ -4,27 +4,56 @@ import passwordCheck
 
 #Create a prompt that asks a user to input their username and password
 
-#takes user input for username and password
-#stores concatenated user and password separated by a space into a file
+#Takes user input for username and password
+#Stores concatenated user and password separated by a space into a file
 
-#dependent on 3 functions each imported from their own file
+#Dependent on 3 functions each imported from their own file
 
 def login():
     fileWrite = open("users.txt", "a")
 
-    user = input("Enter username: ")
+    loginSelection = int(input("Select 1 to login to an existing account\nSelect 2 to register a new account\nSelection: "))
 
-    if accountCheck.accountExist(user) == 1: return
-    if accountCount.accountLimit() >= 5:
-        print("All permitted accounts have been created, please come back and try later.")
-        return
+    #For an existing account
+    if loginSelection != 1:
+        user = input("Enter username: ")
 
-    password = input("Enter password: ")
-    while passwordCheck.securePassword(password) != 1:
+        if accountCheck.accountExist(user) == 1: return
+        if accountCount.accountLimit() >= 5:
+            print("All permitted accounts have been created, please come back and try later.")
+            return
+
+        password = input("Enter password: ")
+        while passwordCheck.securePassword(password) != 1:
+            password = input("Enter password: ")
+
+        combiUserPass = user + " " + password + "\n"
+        fileWrite.write(combiUserPass)
+        fileWrite.close()
+    
+    #For a new account creation
+    else:
+        exitLoop = 0
+        fileOpen = open("users.txt", "r")
+
+        user = input("Enter username: ")
         password = input("Enter password: ")
 
-    combiUserPass = user + " " + password + "\n"
-    fileWrite.write(combiUserPass)
-    fileWrite.close()
-            
+        
+        while exitLoop != 1:
+            for elements in fileOpen:
+                users = elements.split()
+                for names in range(len(users)):
+                    if(user != users[0] and password != users[1]):
+                        print("Incorrect username / password, please try again.")
+                        user = input("Enter username: ")
+                        password = input("Enter password: ")
+                    else:
+                        exitLoop = 1
+                        break
+
+        fileOpen.close()
+        print("You have successfully logged in.")
+                    
 login()
+
