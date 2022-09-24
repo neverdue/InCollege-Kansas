cd71a0461cc4bef34f7d2cf208b013bfada590d6
Mayank Pandey <mp.pandey86@gmail.com>
2022-09-24 17:45:26 -0400

    Made the old tests work again

--- Code/Source/accountCheck.py
+++ Code/Source/accountCheck.py
@@ -1,7 +1,12 @@
 #Checks if the username is already taken
 import json
-def accountExist(username):
-    with open("accounts.json", "r") as json_file:
+def accountExist(username, TESTMODE = False):
+    if TESTMODE == False:
+        dataFile = "accounts.json"
+    else:
+        dataFile = "accounts-test.json"
+
+    with open(dataFile, "r") as json_file:
         data = json.load(json_file)
         for items in data["accounts"]:
             user = items["username"]
