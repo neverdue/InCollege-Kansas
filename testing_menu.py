5ca35a6ef56b60da74973aac37dc7f4c874eca29
Mayank Pandey <mp.pandey86@gmail.com>
2022-10-06 12:21:29 -0400

    Removed temp test file since integration is complete

--- testing_menu.py
+++
@@ -1,8 +0,0 @@
-from Code.Source.menu import *
-from Code.Source.globalVariables import dataFileInit, stackInit
-
-dataFileInit()
-#Initialize stack and add login page
-stackInit()
-# addPage("main")
-incollegeImpLinks()
