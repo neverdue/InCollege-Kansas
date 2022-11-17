0f92635467a2b9ef4004e51d28aed9bbbf8e931d
Boomboozled <103400445+Boomboozled@users.noreply.github.com>
2022-11-17 01:20:54 -0500

    Implemented account registration from api and MyCollege output file features

--- API.py
+++ API.py
@@ -1,3 +1,6 @@
+import json
+from Code.Source.loginPrompt import register
+from Code.Source.globalVariables import dataFileInit, stackInit, getDataFile
 
 def parseStudentAccountInput():
 
@@ -36,10 +39,62 @@ def parseStudentAccountInput():
         print("No API Input: studentsAccounts.txt was not found")
 
     # print("List of students to be added: ",students)
-    print(students)
+    # print(students)
+    return students
+
+#registers users from Input API data
+def runInputAPI():
+    for student in parseStudentAccountInput():
+        # print(student)
+        register(student["username"], student["password"], student["firstname"], student["lastname"], False)
+    return
+
+def createMyCollege_usersOutput():
+    print()
+    #placeholder function for another time
+    outputFile = "MyCollege_user.txt"
+
+    f = open(outputFile, "w") 
+
+    with open(getDataFile()) as json_file:
+        data = json.load(json_file)
+        for user in data["accounts"]:
+            subscription = ""
+            if(user["subscription"] == "False"):
+                subscription = "standard"
+            else:
+                subscription = "plus"
+            
+            f.write(user["username"] + ' ' + subscription + "\n")
+            f.write("=====\n")
+
+    f.close()
+    return
+
+def createMyCollege_profilesOutput():
+    print()
+    #placeholder function for another time
+    outputFile = "MyCollege_profiles.txt"
+
+    f = open(outputFile, "w") 
+
+    with open(getDataFile()) as json_file:
+        data = json.load(json_file)
+        for user in data["accounts"]:
+            #need some way to check if they have profile, and stuff like that.
+            # if ":
+            #     f.write("=====\n")
+            print()
+    f.close()
+    return
+
+#def register(username, password, first, last, subscription):
 
 def main():
-    parseStudentAccountInput()
+    dataFileInit()
+    stackInit()
+    # runInputAPI()
+    createMyCollege_usersOutput()
     return
 
 
