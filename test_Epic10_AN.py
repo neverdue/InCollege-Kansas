499d4f46a39e63108c2ce60a19e3ed9b2bf2780d
spoopyando <nguyena5@usf.edu>
2022-11-20 20:16:47 -0500

    added some tests

--- test_Epic10_AN.py
+++ test_Epic10_AN.py
@@ -3,9 +3,11 @@ import pytest
 import json
 import datetime
 from Code.Source.globalVariables import addPage, dataFileInit, getFirst, getUser, getUserProfile, setExperienceInfo, stackInit, userInit, logout, getJobFile
-from Code.Source.homePageOptions import createProfile
+from Code.Source.homePageOptions import addJobPost, createProfile, deleteJobMenu
 from Code.Source.inputAPI import inputAPIs
+from Code.Source.loginPrompt import signUpPage
 from Code.Source.menu import homePage
+from Code.Source.menuOptions import back
 from Code.Source.outputAPI import outputAPIs
 from Code.Source.mainPage import mainPage
 from Code.Source.utility import writeJson
@@ -143,17 +145,7 @@ def setup():
             ],
             "profile": {
                 "experience": [],
-                "education": [
-                    {
-                        "school name": "TESTusf",
-                        "degree": "TESTcs",
-                        "years attended": "TEST4"
-                    }
-                ],
-                "title": "TEST TITLE EXAMPLE",
-                "major": "TEST MAJOR EXAMPLE",
-                "university": "TEST UNIVERSITY EXAMPLE",
-                "about": "TEST PARAGRAPH ABOUT EXAMPLE"
+                "education": [],
             },
             "registrationTime": "11/03/2022 23:59:59",
             "lastLogin": "11/03/2022 23:59:59"
@@ -223,26 +215,6 @@ def setup():
             },
             "registrationTime": "11/06/2022 23:59:59",
             "lastLogin": "11/06/2022 23:59:59"
-            },
-            {
-            "username": "user7",
-            "password": "Password123!",
-            "firstName": "Mayank",
-            "lastName": "Pandey",
-            "language": "English",
-            "email": "True",
-            "SMS": "True",
-            "ads": "True",
-            "subscription": "True",
-            "incomingRequests": [],
-            "outgoingRequests": [],
-            "friendsList": [],
-            "profile": {
-                "experience": [],
-                "education": []
-            },
-            "registrationTime": "11/09/2022 13:58:30",
-            "lastLogin": creationTime
             }
         ]
     }
@@ -302,137 +274,199 @@ def setup():
     }
     writeJson(fileContents, APPLICATIONFILE)
 
-# def test_outputAppliedJobs():
-#     # Adding to newJobs-test.txt file
-#     stringInput = "TestTitle\nTestDescription\n&&&\nTestFName TestLName\nTestLocation\nTestLocation2\n$00000\n=====\n"
-#     tokenizedInput = ["TestTitle\n", "TestDescription\n", "&&&\n", "TestFName TestLName\n", "TestLocation\n", "TestLocation2\n", "$00000\n", "=====\n"]
-#     lengthList = len(tokenizedInput)
-#     matchCount = 0
+def test_outputAppliedJobs():
+    # Adding to newJobs-test.txt file
+    stringInput = "TestTitle\nTestDescription\n&&&\nTestFName TestLName\nTestLocation\nTestLocation2\n$00000\n=====\n"
+    tokenizedInput = ["TestTitle\n", "TestDescription\n", "&&&\n", "TestFName TestLName\n", "TestLocation\n", "TestLocation2\n", "$00000\n", "=====\n"]
+    lengthList = len(tokenizedInput)
+    matchCount = 0
 
-#     # Preventing duplicates from being written in the test file 
-#     with open(NEW_JOB_APIFILE, "r") as file:
-#         for items in tokenizedInput:
-#             if items in file:
-#                 matchCount+=1
+    # Preventing duplicates from being written in the test file 
+    with open(NEW_JOB_APIFILE, "r") as file:
+        for items in tokenizedInput:
+            if items in file:
+                matchCount+=1
 
-#     if matchCount != lengthList:
-#         with open(NEW_JOB_APIFILE, "a") as txtFile:
-#             txtFile.write(stringInput)
-#         txtFile.close()
-#     file.close()
+    if matchCount != lengthList:
+        with open(NEW_JOB_APIFILE, "a") as txtFile:
+            txtFile.write(stringInput)
+        txtFile.close()
+    file.close()
 
-#     inputAPIs()
-#     outputAPIs()
+    inputAPIs()
+    outputAPIs()
     
-#     # Assertion
-#     with open(MYCOLLEGE_APPLIEDJOBS, "r") as txtFile:
-#         assert "TestTitle\n" in txtFile  
-#     txtFile.close()
+    # Assertion
+    with open(MYCOLLEGE_APPLIEDJOBS, "r") as txtFile:
+        assert "TestTitle\n" in txtFile  
+    txtFile.close()
 
-# def test_outputJobs():
-#     # Adding to newJobs-test.txt file
-#     stringInput = "TestTitle\nTestDescription\n&&&\nTestFName TestLName\nTestLocation\nTestLocation2\n$00000\n=====\n"
-#     tokenizedInput = ["TestTitle\n", "TestDescription\n", "&&&\n", "TestFName TestLName\n", "TestLocation\n", "TestLocation2\n", "$00000\n", "=====\n"]
-#     lengthList = len(tokenizedInput)
-#     matchCount = 0
+def test_outputJobs():
+    # Adding to newJobs-test.txt file
+    stringInput = "TestTitle\nTestDescription\n&&&\nTestFName TestLName\nTestLocation\nTestLocation2\n$00000\n=====\n"
+    tokenizedInput = ["TestTitle\n", "TestDescription\n", "&&&\n", "TestFName TestLName\n", "TestLocation\n", "TestLocation2\n", "$00000\n", "=====\n"]
+    lengthList = len(tokenizedInput)
+    matchCount = 0
 
-#     # Preventing duplicates from being written in the test file 
-#     with open(NEW_JOB_APIFILE, "r") as file:
-#         for items in tokenizedInput:
-#             if items in file:
-#                 matchCount+=1
+    # Preventing duplicates from being written in the test file 
+    with open(NEW_JOB_APIFILE, "r") as file:
+        for items in tokenizedInput:
+            if items in file:
+                matchCount+=1
 
-#     if matchCount != lengthList:
-#         with open(NEW_JOB_APIFILE, "a") as txtFile:
-#             txtFile.write(stringInput)
-#         txtFile.close()
-#     file.close()
+    if matchCount != lengthList:
+        with open(NEW_JOB_APIFILE, "a") as txtFile:
+            txtFile.write(stringInput)
+        txtFile.close()
+    file.close()
 
-#     inputAPIs()
-#     outputAPIs()
+    inputAPIs()
+    outputAPIs()
 
-#     # Assertion
-#     stringList = ['TestTitle\n', 'TestDescription\n', '&&&\n', 'TestLocation\n', 'TestLocation2\n', '$00000\n', '=====\n']
-#     with open(MYCOLLEGE_JOBS, "r") as txtFile:
-#         for items in stringList:
-#             assert items in txtFile
-#     txtFile.close()
+    # Assertion
+    stringList = ['TestTitle\n', 'TestDescription\n', '&&&\n', 'TestLocation\n', 'TestLocation2\n', '$00000\n', '=====\n']
+    with open(MYCOLLEGE_JOBS, "r") as txtFile:
+        for items in stringList:
+            assert items in txtFile
+    txtFile.close()
 
-# # Hard codes data and overwrites accounts-test.json on lines 49-88. Uses those hard coded values for assertions
-# def test_outputProfiles():
-#     assertionList = ['PROFILE TITLE EXAMPLE\n', 'PROFILE MAJOR EXAMPLE\n', 'PROFILE UNIVERSITY EXAMPLE\n', 'PARAGRAPH ABOUT EXAMPLE\n', 'usf cs 4 \n', '=====\n']
-#     with open(MYCOLLEGE_PROFILES, "r") as txtFile:
-#         for items in assertionList:
-#             assert items in txtFile
-#     txtFile.close()
+# Hard codes data and overwrites accounts-test.json on lines 49-88. Uses those hard coded values for assertions
+def test_outputProfiles():
+    assertionList = ['PROFILE TITLE EXAMPLE\n', 'PROFILE MAJOR EXAMPLE\n', 'PROFILE UNIVERSITY EXAMPLE\n', 'PARAGRAPH ABOUT EXAMPLE\n', 'usf cs 4 \n', '=====\n']
+    with open(MYCOLLEGE_PROFILES, "r") as txtFile:
+        for items in assertionList:
+            assert items in txtFile
+    txtFile.close()
 
 
-# # Hard codes data and overwrites applications-test.json on lines 90-142. Uses those hard coded values for assertions
-# def test_outputSavedJobs():
-#     assertionList = ['user1,Job1\n', '=====\n']
-#     with open(MYCOLLEGE_SAVEDJOBS, "r") as txtFile:
-#         for items in assertionList:
-#             assert items in txtFile
-#     txtFile.close()
+# Hard codes data and overwrites applications-test.json on lines 90-142. Uses those hard coded values for assertions
+def test_outputSavedJobs():
+    assertionList = ['user1,Job1\n', '=====\n']
+    with open(MYCOLLEGE_SAVEDJOBS, "r") as txtFile:
+        for items in assertionList:
+            assert items in txtFile
+    txtFile.close()
 
-# def test_outputUsers():
-#     assertionList = ['user1 standard\n', '=====\n']
-#     with open(MYCOLLEGE_USER, "r") as txtFile:
-#         for items in assertionList:
-#             assert items in txtFile
-#     txtFile.close()
+def test_outputUsers():
+    assertionList = ['user1 standard\n', '=====\n']
+    with open(MYCOLLEGE_USER, "r") as txtFile:
+        for items in assertionList:
+            assert items in txtFile
+    txtFile.close()
 
-# # Checking to see if the files have been created
-# def test_profilesCheck():
-#     assert open(MYCOLLEGE_PROFILES)
+# Checking to see if the files have been created
+def test_profilesCheck():
+    assert open(MYCOLLEGE_PROFILES)
 
-# def test_savedJobsCheck():
-#     assert open(MYCOLLEGE_SAVEDJOBS)
+def test_savedJobsCheck():
+    assert open(MYCOLLEGE_SAVEDJOBS)
 
-# def test_JobsCheck():
-#     assert open(MYCOLLEGE_JOBS)
+def test_JobsCheck():
+    assert open(MYCOLLEGE_JOBS)
 
-# def test_appliedJobsCheck():
-#     assert open(MYCOLLEGE_APPLIEDJOBS)
+def test_appliedJobsCheck():
+    assert open(MYCOLLEGE_APPLIEDJOBS)
 
-# def test_usersCheck():
-#     assert open(MYCOLLEGE_USER)
+def test_usersCheck():
+    assert open(MYCOLLEGE_USER)
 
-#["TESTTITLE", "y", "TESTMAJOR", "y", "TESTUNI", "y", "TESTPARA", "n", "y", "TESTUNI", "TESTDEGREE", "1/11/1111", "4"]
 #When a user creates a profile, the profile should be added to MyCollege_profiles.txt
-#@pytest.fixture
 @pytest.mark.parametrize("inputs", 
 [
     (
-        ["1", "testuser1", "Password123@", "10", "1", "TESTTITLE", "y", "TESTMAJOR", "y", "TESTUNI", "y", "TESTPARA", "n", "y", "TESTUNI", "TESTDEGREE", "1/11/1111", "4"]
+        ["TESTTITLE", "y", "TESTMAJOR", "y", "TESTUNI", "y", "TESTPARA", "y", "y", "TEST", "TEST", "11/11/1111", "11/11/1111", "TEST", "TEST", "n", "y", "TEST", "TEST", "4"]
     )
 ]
 )
 def test_createProfile(monkeypatch, inputs):
     try:
         monkeypatch.setattr('sys.stdin', io.StringIO('\n'.join(inputs)))
-        main()
-        #createProfile()
-        setExperienceInfo("TESTEXPERIENCE")
+        createProfile()
         inputAPIs()
         outputAPIs()
     except EOFError:
-        # with open(MYCOLLEGE_PROFILES, "r") as txtFile:
-        #     assert 1==1
-        assert 1 == 1
+        #"Testmajor\n", "Testuni\n", "TESTPARA\n", "TEST TEST 11/11/1111 11/11/1111 TEST TEST\n", "TEST TEST 4\n", "=====\n"
+        assertList = ["TESTTITLE\n", "Testmajor\n", "Testuni\n", "TESTPARA\n", "TEST TEST 11/11/1111 11/11/1111 TEST TEST \n", "TEST TEST 4 \n", "=====\n"]
+        with open(MYCOLLEGE_PROFILES, "r") as txtFile:
+            for tokens in assertList:
+                assert tokens in txtFile
+        txtFile.close()
 
-# def test_createProfile():
-#     assertList = [[],[{'school name': 'TESTusf', 'degree': 'TESTcs', 'years attended': 'TEST4'}], "TEST TITLE EXAMPLE", "TEST MAJOR EXAMPLE", "TEST UNIVERSITY EXAMPLE", "TEST PARAGRAPH ABOUT EXAMPLE"]
-#     inputAPIs()
-#     outputAPIs()
-#     with open(DATAFILE) as json_file:
-#         data = json.load(json_file)
-#         for items in data["accounts"]:
-#             if items["username"] == getUser():
-#                 for stuff in items["profile"]:
-#                     print(items["profile"][stuff])
-#                     for tokens in assertList:
-#                         print("tokens: ", tokens, " items[profile][stuff]: ", items["profile"][stuff])
-#                         assert tokens in items["profile"][stuff]
-#     assert 1 == 1
+# When a job is created, add to MyCollege_jobs.txt
+@pytest.mark.parametrize("inputs", 
+[
+    (
+        ["TESTJOB", "TESTDESCRIPTION", "TESTEMPLOYER", "TESTLOCATION", "$00000", "n"]
+    )
+]
+)
+def test_createJob(monkeypatch, inputs):
+    monkeypatch.setattr('sys.stdin', io.StringIO('\n'.join(inputs)))
+    addJobPost()
+    inputAPIs()
+    outputAPIs()
+
+    assertList = ["TESTJOB\n", "TESTDESCRIPTION\n", "&&&\n", "TESTEMPLOYER\n", "TESTLOCATION\n", "$00000\n", "=====\n"]
+    with open(MYCOLLEGE_JOBS) as txtFile:
+        for tokens in assertList:
+            assert tokens in txtFile
+
+    txtFile.close()
+
+# When a new user creates an account, add the user account to MyCollege_users.txt
+@pytest.mark.parametrize("inputs", 
+[
+    (
+        ["burrito", "Password123!", "burrito", "tran", "no"]
+    )
+]
+)
+def test_createAccount(monkeypatch, inputs):
+    monkeypatch.setattr('sys.stdin', io.StringIO('\n'.join(inputs)))
+    logout()
+    signUpPage()
+    inputAPIs()
+    outputAPIs()
+
+    assertList = ["burrito standard\n", "=====\n"]
+    with open(MYCOLLEGE_USER) as txtFile:
+        for tokens in assertList:
+            assert tokens in txtFile
+    txtFile.close()
+
+# When a job is deleted, recreate MyCollege_jobs.txt with deleted job removed
+@pytest.mark.parametrize("inputs", 
+[
+    (
+        ["1"]
+    )
+]
+)
+def test_deleteJob(monkeypatch, inputs):
+    #try:
+        stackInit()
+        addPage()
+        monkeypatch.setattr('sys.stdin', io.StringIO((inputs)))
+        jobDict = {
+                "id": "8",
+                "Title": "Job8",
+                "Description": "save pytest",
+                "Employer": "Apple",
+                "Location": "Silicon Valley",
+                "Salary": "$300,000",
+                "Name": "tommy truong",
+                "TimeCreated": "11/01/2022 23:59:59"
+            }
+        deleteJobMenu(jobDict)
+        #inputAPIs()
+        outputAPIs()
+
+    #except EOFError:
+        assertList = ["Job8"]
+        with open(MYCOLLEGE_USER) as txtFile:
+            for tokens in assertList:
+                assert tokens not in txtFile
+        txtFile.close()
+
+
 
